79c3720acd99350f0195e91495284413778157ab
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-31 16:36:38 +0400

    with pre-commit

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
-.DEFAULT_GOAL := help
-
 .PHONY: venv
 venv: ## Create the virtual environment
 	@curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -10,6 +8,12 @@ venv: ## Create the virtual environment
 install: venv ## Install a virtual environment
 	@uv pip install --upgrade pip
 	@uv pip install -r requirements.txt
+
+
+.PHONY: help
+help:  ## Display this help screen
+        @echo -e "\033[1mAvailable commands:\033[0m"
+        @grep -E '^[a-z.A-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%-18s\033[0m %s\n", $$1, $$2}' | sort
 
 
 .PHONY: fmt
@@ -25,17 +29,18 @@ clean:  ## Clean up caches and build artifacts
 	@git branch -v | grep "\[gone\]" | cut -f 3 -d ' ' | xargs git branch -D
 
 
-.PHONY: help
-help:  ## Display this help screen
-	@echo -e "\033[1mAvailable commands:\033[0m"
-	@grep -E '^[a-z.A-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%-18s\033[0m %s\n", $$1, $$2}' | sort
-
-
 .PHONY: marimo
 marimo: install ## Install Marimo
 	@uv pip install marimo
 	@uv run marimo edit notebooks
 
+
 .PHONY: backend
 backend: install # Run the fastAPI backend
 	@uv run uvicorn main:app --port 5050
+
+
+#.PHONY: openbb_ui
+#openbb_ui: install # Run open bb#
+#	@uv pip install openbb --no-deps
+#	@uv run openbb
--- main.py
+++ main.py
@@ -2,12 +2,9 @@ import json
 from pathlib import Path
 
 import pandas as pd
-import plotly.graph_objects as go
-import requests
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from plotly_templates import dark_template
 
 app = FastAPI()
 
@@ -15,6 +12,7 @@ origins = [
     "http://localhost",
     "http://localhost:1420",
     "http://localhost:5050",
+    "http://127.0.0.1:5050"
     "https://pro.openbb.dev",
     "https://pro.openbb.co",
     "https://excel.openbb.co",
@@ -39,118 +37,121 @@ def read_root():
 
 @app.get("/widgets.json")
 def get_widgets():
-    """Widgets configuration file for the OpenBB Custom Backend"""
-    return JSONResponse(
-        content=json.load((Path(__file__).parent.resolve() / "widgets.json").open())
-    )
-
-
-@app.get("/chains")
-def get_chains():
-    """Get current TVL of all chains using Defi LLama"""
-    params = {}
-    response = requests.get("https://api.llama.fi/v2/chains", params=params)
-
-    if response.status_code == 200:
-        # Create a DataFrame from the JSON data
-        df = pd.DataFrame(response.json())
-
-        # Sort the DataFrame by 'tvl' in descending order and select the top 30
-        top_30_df = df.sort_values(by='tvl', ascending=False).head(30)
-
-        # Create a bar chart using Plotly
-        figure = go.Figure(
-            data=[go.Bar(x=top_30_df["tokenSymbol"], y=top_30_df["tvl"])],
-            # Apply the dark template - see plotly_templates.py
-            layout=go.Layout(
-                template=dark_template,
-                title="Top 30 Chains by TVL",
-                xaxis_title="Token Symbol",
-                yaxis_title="Total Value Locked (TVL)"
-            )
-        )
-
-        # return the plotly json
-        return json.loads(figure.to_json())
-
-    print(f"Request error {response.status_code}: {response.text}")
-    return JSONResponse(
-        content={"error": response.text}, status_code=response.status_code
-    )
-
-
-@app.get("/historical_chains")
-def get_historical_chains(chain: str = None):
-    """Get historical TVL of a chain using Defi LLama"""
-
-    if chain is None:
-        chain = "Ethereum"
-    response = requests.get(f'https://api.llama.fi/v2/historicalChainTvl/{chain}')
-
-    if response.status_code == 200:
-        return response.json()
-
-    print(f"Request error {response.status_code}: {response.text}")
-    return JSONResponse(
-        content={"error": response.text}, status_code=response.status_code
-    )
-
-# Fetching list of chains for a parameter in the widget
-@app.get("/get_chains_list")
-def get_chains_list():
-    """Get list of chains using Defi LLama"""
-    response = requests.get("https://api.llama.fi/v2/chains")
-
-    if response.status_code == 200:
-        data = response.json()
-        # can pass as list of {label, value} for dropdown or list of strings
-        #  [
-        #   {"label": chain.get("name"), "value": chain.get("name")}
-        #   for chain in data if chain.get("name")
-        #  ]
-        return [chain.get("name") for chain in data if chain.get("name")]
-
-    print(f"Request error {response.status_code}: {response.text}")
-    return JSONResponse(
-        content={"error": response.text}, status_code=response.status_code
-    )
-
-@app.get("/show_example_params")
-def show_example_params(datePicker1: str = None, textBox1: str = None, daysPicker1: str = "1"):
-    """Show example of how to use parameters in the URL"""
-
-    return {"datePicker1": datePicker1, "textBox1": textBox1, "daysPicker1": daysPicker1.split(",")}
-
-
-@app.get("/chains_table")
-def chains_table():
-    """Get current TVL of all chains using Defi LLama"""
-    params = {}
-    response = requests.get("https://api.llama.fi/v2/chains", params=params)
-
-    if response.status_code == 200:
-        return response.json()
-
-    print(f"Request error {response.status_code}: {response.text}")
-    return JSONResponse(
-        content={"error": response.text}, status_code=response.status_code
-    )
-
-
-@app.get("/json-data")
-def json_data():
-    """Read mock csv data and return it as a table to your widget"""
-    # Specify the path to your JSON file
-    json_file_path = "mock_data.json"
-
-    try:
-        # Return the JSON data as is
-        return json.load((ROOT_PATH / json_file_path).open()).get("stocks", [])
-    except Exception as e:
-        # Handle error cases here
-        error_message = f"Error reading the JSON file: {str(e)}"
-        return JSONResponse(content={"error": error_message}, status_code=500)
-
+    """Widgets configuration file for OpenBB Pro"""
+    file_path = "widgets.json"
+    with open(file_path) as file:
+        data = json.load(file)
+    return JSONResponse(content=data)
+#
+#
+#
+#
+# @app.get("/chains")
+# def get_chains():
+#     """Get current TVL of all chains using Defi LLama"""
+#     params = {}
+#     response = requests.get("https://api.llama.fi/v2/chains", params=params)
+#
+#     if response.status_code == 200:
+#         # Create a DataFrame from the JSON data
+#         df = pd.DataFrame(response.json())
+#
+#         # Sort the DataFrame by 'tvl' in descending order and select the top 30
+#         top_30_df = df.sort_values(by='tvl', ascending=False).head(30)
+#
+#         # Create a bar chart using Plotly
+#         figure = go.Figure(
+#             data=[go.Bar(x=top_30_df["tokenSymbol"], y=top_30_df["tvl"])],
+#             # Apply the dark template - see plotly_templates.py
+#             layout=go.Layout(
+#                 template=dark_template,
+#                 title="Top 30 Chains by TVL",
+#                 xaxis_title="Token Symbol",
+#                 yaxis_title="Total Value Locked (TVL)"
+#             )
+#         )
+#
+#         # return the plotly json
+#         return json.loads(figure.to_json())
+#
+#     print(f"Request error {response.status_code}: {response.text}")
+#     return JSONResponse(
+#         content={"error": response.text}, status_code=response.status_code
+#     )
+#
+#
+# @app.get("/historical_chains")
+# def get_historical_chains(chain: str = None):
+#     """Get historical TVL of a chain using Defi LLama"""
+#
+#     if chain is None:
+#         chain = "Ethereum"
+#     response = requests.get(f'https://api.llama.fi/v2/historicalChainTvl/{chain}')
+#
+#     if response.status_code == 200:
+#         return response.json()
+#
+#     print(f"Request error {response.status_code}: {response.text}")
+#     return JSONResponse(
+#         content={"error": response.text}, status_code=response.status_code
+#     )
+#
+# # Fetching list of chains for a parameter in the widget
+# @app.get("/get_chains_list")
+# def get_chains_list():
+#     """Get list of chains using Defi LLama"""
+#     response = requests.get("https://api.llama.fi/v2/chains")
+#
+#     if response.status_code == 200:
+#         data = response.json()
+#         # can pass as list of {label, value} for dropdown or list of strings
+#         #  [
+#         #   {"label": chain.get("name"), "value": chain.get("name")}
+#         #   for chain in data if chain.get("name")
+#         #  ]
+#         return [chain.get("name") for chain in data if chain.get("name")]
+#
+#     print(f"Request error {response.status_code}: {response.text}")
+#     return JSONResponse(
+#         content={"error": response.text}, status_code=response.status_code
+#     )
+#
+# @app.get("/show_example_params")
+# def show_example_params(datePicker1: str = None, textBox1: str = None, daysPicker1: str = "1"):
+#     """Show example of how to use parameters in the URL"""
+#
+#     return {"datePicker1": datePicker1, "textBox1": textBox1, "daysPicker1": daysPicker1.split(",")}
+#
+#
+# @app.get("/chains_table")
+# def chains_table():
+#     """Get current TVL of all chains using Defi LLama"""
+#     params = {}
+#     response = requests.get("https://api.llama.fi/v2/chains", params=params)
+#
+#     if response.status_code == 200:
+#         return response.json()
+#
+#     print(f"Request error {response.status_code}: {response.text}")
+#     return JSONResponse(
+#         content={"error": response.text}, status_code=response.status_code
+#     )
+#
+#
+# @app.get("/json-data")
+# def json_data():
+#     """Read mock csv data and return it as a table to your widget"""
+#     # Specify the path to your JSON file
+#     json_file_path = "mock_data.json"
+#
+#     try:
+#         # Return the JSON data as is
+#         return json.load((ROOT_PATH / json_file_path).open()).get("stocks", [])
+#     except Exception as e:
+#         # Handle error cases here
+#         error_message = f"Error reading the JSON file: {str(e)}"
+#         return JSONResponse(content={"error": error_message}, status_code=500)
+#
 
 @app.get("/csv-data")
 def csv_data():
